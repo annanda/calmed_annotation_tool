0e257ddb04ac3d9925367953df3e582d9f5d948f
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-02-20 12:57:10 +0000

    Extending the behaviour to all buttons while sending the right data to the DB.

--- static/emotions.js
+++ static/emotions.js
@@ -11,15 +11,11 @@ function make_request(video_name, emotion, time_seconds, behaviors, method = "po
         contentType: 'application/json',
         processData: false,
         success: function (msg) {
-            // document.getElementById("answer_mood").style.display = 'block';
-            // let text =  'Thank you! <br>';
-            // document.getElementById("answer_mood").innerHTML= text + '<a id="continue_mood_link" href="/'+ next_page + '" > Let\'s CONTINUE!';
-            document.getElementById("behaviours").style.display = 'block';
+            document.getElementById("behaviours").style.display = 'none';
+            document.getElementById("wrapper").style.display = 'block';
+
+            document.getElementById('video_annotation').currentTime = time_seconds;
             document.getElementById('video_annotation').play()
-            // alert("Thank you!");
-            // setTimeout(function () {
-            //     document.getElementById('video_annotation').play()
-            // }, 2000);
         }
         // async: false
     });
@@ -27,14 +23,22 @@ function make_request(video_name, emotion, time_seconds, behaviors, method = "po
 }
 
 function collect_behaviours(video_file, emotion, time_seconds) {
-    behaviours = {
-        'jump': 1,
-        'laugh': 0,
-        'head_movement': 0,
-        'other': 'Sending data by JS'
-    }
+    behaviours = {}
 
-    // document.getElementById("emotion_blue").onclick =
+    var behaviour = document.getElementById("behaviours_checkbox")
+    var txt = "";
+    var i;
+    for (i = 0; i < behaviour.length - 1; i++) {
+        if (behaviour[i].checked) {
+            behaviours[behaviour[i].value] = 1
+        } else {
+            behaviours[behaviour[i].value] = 0
+        }
+    }
+    var typed_behaviour = document.getElementById('typed_text').value
+    behaviours['Other'] = typed_behaviour;
+    $('input[type=checkbox]').prop('checked', false);
+    document.getElementById('typed_text').value = "";
     return behaviours
 }
 
@@ -52,18 +56,39 @@ window.onload = function () {
         time_seconds = video.currentTime;
         video.pause();
         document.getElementById("behaviours").style.display = 'block';
+        document.getElementById("wrapper").style.display = 'none';
         video_name = document.getElementById("source_video").getAttribute('src');
-        // behaviours = {
-        //     'jump': 1,
-        //     'laugh': 0,
-        //     'head_movement': 0,
-        //     'other': 'Sending data by JS'
-        // }
+    }
+
+    document.getElementById("emotion_green").onclick = function () {
+        emotion = 'green';
+        time_seconds = video.currentTime;
+        video.pause();
+        document.getElementById("behaviours").style.display = 'block';
+        document.getElementById("wrapper").style.display = 'none';
+        video_name = document.getElementById("source_video").getAttribute('src');
+    }
+
+    document.getElementById("emotion_red").onclick = function () {
+        emotion = 'red';
+        time_seconds = video.currentTime;
+        video.pause();
+        document.getElementById("behaviours").style.display = 'block';
+        document.getElementById("wrapper").style.display = 'none';
+        video_name = document.getElementById("source_video").getAttribute('src');
+    }
+
+    document.getElementById("emotion_yellow").onclick = function () {
+        emotion = 'yellow';
+        time_seconds = video.currentTime;
+        video.pause();
+        document.getElementById("behaviours").style.display = 'block';
+        document.getElementById("wrapper").style.display = 'none';
+        video_name = document.getElementById("source_video").getAttribute('src');
     }
 
     document.getElementById("send_behaviours").onclick = function () {
         behaviours = collect_behaviours();
         make_request(video_name, emotion, time_seconds, behaviours);
-
     }
 }
