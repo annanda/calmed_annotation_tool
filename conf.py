2052e881da23a08c492f86e6fa56bc265b451733
Annanda Sousa <annanda.sousa@insight-centre.org>
2020-10-27 11:42:49 +0000

    Adding README and the db for 2 small videos annotation.

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
 from decouple import config
 
 # VIDEO_FILE = config('VIDEO_FILE', default='to_annotate/ED_3_makes_sad.mp4')
-# VIDEO_FILE = config('VIDEO_FILE', default='to_annotate/ED_dataset_makes_sad_small.mp4')
-VIDEO_FILE = config('VIDEO_FILE', default='to_annotate/ED_dataset_wish_small.mp4')
+VIDEO_FILE = config('VIDEO_FILE', default='to_annotate/ED_dataset_makes_sad_small.mp4')
+# VIDEO_FILE = config('VIDEO_FILE', default='to_annotate/ED_dataset_wish_small.mp4')
---
+++ working_dataset_creation/README.md
@@ -0,0 +1,4 @@
+# How to generate working datasets from video annotation in the database
+
+1. Extract the content of the database to a CSV file
+* ```sqlite3 video_annotation.db```
