69028c3a0b74dbec3030dae304012c3b3b0c7e26
Annanda Sousa <annanda.sousa@insight-centre.org>
2021-04-13 13:36:03 +0100

    Improving the generating working dataset by separating the videos by directories.

--- working_dataset_creation/generate_working_dataset.py
+++ working_dataset_creation/generate_working_dataset.py
@@ -5,11 +5,11 @@ import cv2
 import glob
 
 main_folder = pathlib.Path(__file__).parent.parent.absolute()
-path_videos_directory = os.path.join(main_folder, 'static', 'to_annotate')
+path_videos_directory = os.path.join(main_folder, 'static', 'videos')
 
 
-def get_video_duration(filename):
-    video_path = os.path.join(path_videos_directory, filename)
+def get_video_duration(filename, folder_name):
+    video_path = os.path.join(path_videos_directory, folder_name, filename)
     video = cv2.VideoCapture(video_path)
     frame_count = video.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = video.get(cv2.CAP_PROP_FPS)
@@ -72,35 +72,44 @@ def create_df_from_time_entries(video_duration, list_times):
     return new_df
 
 
-def split_annotation_by_video_files(annotation_file):
+def split_annotation_by_video_files(annotation_file, target_video):
     annotation_df_from_db = pd.read_csv(annotation_file)
     video_files = annotation_df_from_db.video_file_name.unique()
     output_folder = pathlib.Path(__file__).parent.absolute()
-    output_folder = os.path.join(output_folder, 'output_from_db', 'by_video_file')
+    output_folder = os.path.join(output_folder, 'output_from_db', 'by_video_file', target_video)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     for video in video_files:
-        new_df = annotation_df_from_db[annotation_df_from_db['video_file_name'] == video]
-        df_len = len(new_df)
-        new_indexes = list(range(0, df_len))
-        new_df.index = new_indexes
-        csv_name = f'{output_folder}/{video}.csv'
-        new_df.to_csv(csv_name, index=True)
+        if target_video in video:
+            new_df = annotation_df_from_db[annotation_df_from_db['video_file_name'] == video]
+            df_len = len(new_df)
+            new_indexes = list(range(0, df_len))
+            new_df.index = new_indexes
+            csv_name = f'{output_folder}/{video}.csv'
+            new_df.to_csv(csv_name, index=True)
 
 
-def create_working_datasets(annotation_file):
-    split_annotation_by_video_files(annotation_file)
-    annotation_per_video_files = glob.glob(f'{main_folder}/working_dataset_creation/output_from_db/by_video_file/*.csv')
+def create_working_datasets(annotation_file, target_video):
+    # split_annotation_by_video_files(annotation_file, target_video)
+    annotation_per_video_files = glob.glob(
+        f'{main_folder}/working_dataset_creation/output_from_db/by_video_file/{target_video}/*.csv')
     for annotation_video_path in annotation_per_video_files:
         video_name = annotation_video_path.split('/')[-1]
         video_name = video_name.split('.csv')[0]
-        time_video = get_video_duration(video_name)
+        folder_name = target_video
+        time_video = get_video_duration(video_name, folder_name)
         list_times = generate_dataset_entries(time_video)
         output_df = create_df_from_time_entries(time_video, list_times)
         working_dataset = fill_emotions_from_time(output_df, annotation_video_path)
-        output_folder_path = os.path.join(main_folder, 'working_dataset')
+        output_folder_path = os.path.join(main_folder, 'working_dataset', target_video)
+        if not os.path.exists(output_folder_path):
+            os.makedirs(output_folder_path)
         working_dataset.to_csv(f'{output_folder_path}/{video_name}.csv')
 
 
 if __name__ == '__main__':
     # annotation_file = '/Users/user/PycharmProjects/annotation_tool/working_dataset_creation/output_from_db/data_annotation_time_test.csv'
-    annotation_file = '/Users/user/PycharmProjects/annotation_tool/working_dataset_creation/output_from_db/data_annotation_videos_small.csv'
-    create_working_datasets(annotation_file)
+    annotation_file = '/Users/user/PycharmProjects/annotation_tool/working_dataset_creation/output_from_db/pilot_1.csv'
+    target_video = 'pilots_1'
+    split_annotation_by_video_files(annotation_file, target_video)
+    create_working_datasets(annotation_file, target_video)
