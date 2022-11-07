74ef21cdb42e6f95db6c406aa3691cce9560022c
Annanda Sousa <annanda.sousa@insight-centre.org>
2022-11-07 09:40:00 +0000

    Including name of video above the video for annotation. (2)

--- app.py
+++ app.py
@@ -91,6 +91,6 @@ if __name__ == '__main__':
     app.app_context().push()
     # db.drop_all()
     # db.create_all()
-    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
     # localrun
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    # app.run(debug=True, host='0.0.0.0', port=5050)
